82346954da9792423d2e07679372f9c6d8fb6e42
Daniel Tereschenko <daniel@tereschenko.info>
2014-11-25 12:44:39 +0300

    fix

--- components/validators/validateInn.php
+++ components/validators/validateInn.php
@@ -1,1 +1,42 @@
-/home/user/srv/front.dev/public/yii2/frontend/components/validators/validateInn.php
+<?php
+// INN validator (10-digit for legals, 12-digits for person)
+//  in rules()   [['inn'], 'app\components\validators\validateInn'],
+// for test purpose use fake INN number 1234567894 or 123456789110
+
+namespace frontend\components\validators;
+
+use yii\validators\Validator;
+
+
+class ValidateInn extends Validator
+{
+    public function validateAttribute($model, $attribute)
+    {
+        $inn = $model->$attribute;
+        $len = strlen($inn);
+        if ($len !== 12 || $len !== 10) {
+            $this->addError($model, $attribute, \Yii::t('validate', 'Incorrect INN length'));
+        }
+        
+        if ($len === 10 && ($inn[9] !== (string) (((
+                2*$inn[0] + 4*$inn[1] + 10*$inn[2] + 
+                3*$inn[3] + 5*$inn[4] +  9*$inn[5] + 
+                4*$inn[6] + 6*$inn[7] +  8*$inn[8]
+                ) % 11) % 10))) { 
+            $this->addError($model, $attribute, \Yii::t('validate', 'Incorrect INN checksum'));
+            
+        } elseif ($len === 12 && (($inn[10] !== (string) (((
+                7*$inn[0] + 2*$inn[1] + 4*$inn[2] +
+               10*$inn[3] + 3*$inn[4] + 5*$inn[5] + 
+                9*$inn[6] + 4*$inn[7] + 6*$inn[8] +
+                8*$inn[9]
+                ) % 11) % 10)) || ($inn[11] === (string) (((
+                3*$inn[0] +  7*$inn[1] + 2*$inn[2] +
+                4*$inn[3] + 10*$inn[4] + 3*$inn[5] +
+                5*$inn[6] +  9*$inn[7] + 4*$inn[8] +
+                6*$inn[9] +  8*$inn[10]
+                ) % 11) % 10)))) {            
+            $this->addError($model, $attribute, \Yii::t('validate', 'Incorrect INN checksum'));
+        }
+    }
+}
--- components/validators/validateOgrn.php
+++ components/validators/validateOgrn.php
@@ -1,1 +1,42 @@
-/home/user/srv/front.dev/public/yii2/frontend/components/validators/validateOgrn.php
+<?php
+// OGRN | OGRNIP validator (13-digit for OGRN, 15-digit for OGRNIP)
+//  in rules()   [['ogrn'], 'app\components\validators\validateOgrn'],
+// for test purpose use fake OGRN \ OGRNIP number 1111111111110 | 311111111111115
+
+namespace frontend\components\validators;
+
+use yii\validators\Validator;
+
+
+class ValidateOgrn extends Validator
+{
+    public function validateAttribute($model, $attribute)
+    {
+        $ogrn = $this->parseOgrn($model->$attribute);
+        if ($ogrn['length'] !== 13 && $ogrn['length'] !== 15) {
+            $this->addError($model, $attribute, \Yii::t('validate', 'Incorrect OGRN length'));
+        }
+        if ($ogrn['length'] === 13 && !in_array($ogrn['attrFlag'], ['1', '2', '3', '5']) ||
+            $ogrn['length'] === 15 && !in_array($ogrn['attrFlag'], ['3', '4'])) {
+            $this->addError($model, $attribute, \Yii::t('validate', 'Incorrect OGRN registration attribution flag'));
+        }
+        if ($ogrn['year'] < 2 || $ogrn['year'] > date('y')){
+            $this->addError($model, $attribute, \Yii::t('validate', 'Incorrect OGRN registration year'));
+        }
+        if ($ogrn['checksum'] !== $ogrn['realChecksum']){
+            $this->addError($model, $attribute, \Yii::t('validate', 'Incorrect OGRN checksum'));
+        }
+    }
+    
+    protected function parseOgrn($ogrn)
+    {
+        return [
+            'length' => strlen($ogrn),
+            'attrFlag' => (int) substr($ogrn, 0, 1),
+            'year' => (int) substr($ogrn, 1, 2),
+            'checksum' => (strlen($ogrn) === 13) ? (int) substr($ogrn, 12, 1) : (int) substr($ogrn, 14, 1),
+            'realChecksum' => (strlen($ogrn) === 13) ? (int) substr((substr($ogrn, 0, 12) % 11), -1) :
+                                                       (int) substr((substr($ogrn, 0, 14) % 13), -1),
+        ];
+    }
+}
